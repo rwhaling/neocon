b297489442b67013df939b407d54f0767efadc23
Richard Whaling <rwhaling@spantree.net>
2015-10-02 22:43:40 -0500

    parsing xml

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,31 +4,29 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-import scala.io.Source.fromFile
-
+import scala.io.Source
+import scala.xml._
+import scala.xml.pull._
 class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Hi, Your new application is ready."))
   }
 
-  def results = Action { 
-  	Ok("Results: ")
-  }
-
   def upload = Action(parse.multipartFormData) { request =>  	
-	request.body.file("text") match {
-		case Some(file) => {
-		  val filename = file.filename 
-		  val contentType = file.contentType
-		  val destFile = new File(s"/tmp/upload/$filename")
-		  file.ref.moveTo(destFile)
-		  val lines = scala.io.Source.fromFile(destFile).getLines
-		  Ok("Uploaded " + filename + " :\n" + lines.reduceLeft(_ + _) )
-		}
-		case None => {
-		  	Redirect(routes.Application.index)
-		}
-    }
+  	request.body.file("text").map { text => 
+  		val filename = text.filename
+  		val contentType = text.contentType
+  		val source = Source.fromFile(text.ref.file)
+      var response = "Uploaded " + filename + ": \n"
+      val reader = new XMLEventReader(source)
+      for (event <- reader) event match {
+        case EvText(text) => response = response + text
+        case _ =>         
+      }
+      Ok(response)
+  	}.getOrElse {
+  		Redirect(routes.Application.index)
+  	}
   }
 }
