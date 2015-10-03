8b7a3a2dde51734e4e4374bacab3c04111bfe681
Richard Whaling <rwhaling@spantree.net>
2015-10-03 00:05:19 -0500

    tokenizing

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,8 +10,11 @@ import scala.xml.pull._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Hi, Your new application is ready."))
+    Ok(views.html.index("Upload an XML file."))
   }
+
+  case class Node(pre: Int, size: Int)
+  case class Token(node_pre: Int, content: String, start: Int, end: Int)
 
   def upload = Action(parse.multipartFormData) { request =>  	
   	request.body.file("text").map { text => 
@@ -20,10 +23,46 @@ class Application extends Controller {
   		val source = Source.fromFile(text.ref.file)
       var response = "Uploaded " + filename + ": \n"
       val reader = new XMLEventReader(source)
+      val doc = Node(0,0)
+      var stack = doc :: List()
+      var index: List[Token] = List()
       for (event <- reader) event match {
-        case EvText(text) => response = response + text
-        case _ =>         
+        case EvElemStart(prefix,tag,attrs,scope) => {
+          val parent = stack.head
+          val new_pre = parent.pre + parent.size + 1
+          val new_el = Node(new_pre, 0)
+          stack = new_el :: stack
+          response += s"<$tag pre=$new_pre>"
+          // stack = stack.map(_._1, _._2+1)
+        }
+        case EvText(text) => {
+          val pattern = """\w+""".r
+          response = response + text
+          val matches = pattern.findAllIn(text)
+          var count = 0
+          var tokens: List[String] = List()
+          var tok_string = ""
+          matches.foreach { e =>
+            tokens = text.substring(matches.start(0),matches.end(0)) :: tokens
+            tok_string = tok_string + s"'${matches.group(0)}':${matches.start(0)}:${matches.end(0)}, "
+            val token = Token(stack.head.pre, matches.group(0), matches.start(0),matches.end(0))
+            index = token :: index
+            count += 1
+          }
+          response = response + s"($count tokens$tok_string)"
+        }
+        case EvElemEnd(prefix,tag) => {
+          val done = stack.head
+          stack = stack.tail
+          val parent = stack.head
+          val updated_parent = Node(parent.pre, parent.size + done.size + 1)
+          val dist = done.pre - parent.pre
+          stack = updated_parent :: stack.tail            
+          response += s"</$tag pre=${done.pre} dist=$dist size=${done.size} >"
+        }
+        case _ => {}
       }
+      response += s"${index.length} TOTAL TOKENS"
       Ok(response)
   	}.getOrElse {
   		Redirect(routes.Application.index)
